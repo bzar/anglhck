6f009b8b942cf6483e1d89257aacb50b6d358036
bzar <teemu.erkkola@iki.fi>
2013-05-22 18:21:16 +0300

    Change rendering to AUTO instead of OPENGL

--- test/anglhck_run.cpp
+++ test/anglhck_run.cpp
@@ -44,7 +44,7 @@ int main(int argc, char** argv)
   if (!glhckContextCreate(argc, argv))
      return EXIT_FAILURE;
 
-  if (!glhckDisplayCreate(WIDTH, HEIGHT, GLHCK_RENDER_OPENGL))
+  if (!glhckDisplayCreate(WIDTH, HEIGHT, GLHCK_RENDER_AUTO))
      return EXIT_FAILURE;
 
   run(scriptFile);
